ca74ea06235072f29dbe0b2167e9699307f33404
Ayomide Ogunjobi <azogunjobi21@gmail.com>
2025-03-08 19:07:17 -0800

    Updated UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 //import 'antd/dist/antd.css';
-import Hospitals from './pages/Hospitals';
+//import Hospitals from './pages/Hospitals';
 import SignUp from './pages/SignUp'
 //import About from './pages/About';
 
@@ -12,7 +12,6 @@ const App = () => {
     <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/hospitals" element={<Hospitals />} />
       <Route path="/signup" element={<SignUp/>}/>
     </Routes>
   </Router>
